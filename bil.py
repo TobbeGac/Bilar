b86df5de20e53e71c6cf44fd0c2a0fe28df46e8c
Tobias Gabrielsson <toga0503@storsjogymnasiet.se>
2023-03-17 13:01:06 +0100

    Listan funkar på någe vis

--- bil.py
+++ bil.py
@@ -6,5 +6,15 @@ class Bil:
         self.fabrikat = fabrikat
         self.color = color
         self.lager = lager
-        
-        
+
+    def set_lager(self, lager):
+        self.lager = lager
+    
+    def get_lager(self):
+        return self.lager
+    
+    def get_fabrikat(self, fabrikat):
+        self.fabrikat = fabrikat
+    
+    def get_color(self, color):
+        self.color = color
--- main.py
+++ main.py
@@ -15,7 +15,30 @@ while looping:
     #Skriver ut lista med bilar
     for bil in billista:
         print(str(i+1) + " : " + bil.fabrikat + " : " +bil.color+ ", anta i lager: " + str(bil.lager) + " st")
+        i += 1
 
+    bil_nr = input("\nMata in siffran för vald bil: ")
+    bil_nr_int = int(bil_nr)
+
+    #print("bil nr= " + bil_nr)
+
+    lager_int = billista[bil_nr_int-1].get_lager()
+
+
+    if lager_int > 0:
+        print("\n Här kommer en " + billista[bil_nr_int-1].color + " " + billista[bil_nr_int-1].fabrikat)
+
+        #minskar lagret
+        nytt_lagersaldo_int = lager_int - 1
+        nytt_lagersaldo_string = str(nytt_lagersaldo_int)
+        #Minskar bilobjektet lager
+        billista[bil_nr_int-1].set_lager(nytt_lagersaldo_int)
+
+        
+
+    else:
+        print ("\nSynd, du blir utan bil!\n")
+    print("Lager saldo: " +str( billista[bil_nr_int-1].get_lager()))
     #Avsluta programmet
     go = input("\n Vill du avsluta programmet? j/n ")
 
