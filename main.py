34990f1807d4bdb57b9cf70428b15083b183c3a6
Tobias Gabrielsson <toga0503@storsjogymnasiet.se>
2023-03-17 10:22:31 +0100

    Listan printas ut på terminalen

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ volvo_svart = bil.Bil("Volvo", "Svarta", 5)
 volvo_vit = bil.Bil("Volvo", "Vit", 2)
 ferrari_röd = bil.Bil("Ferrari", "röd", 4)
 
+billista = [volvo_svart, volvo_vit, ferrari_röd]
+
 while looping:
     print("-----------------------------------------")
     print("\n-:BilAutomat:-\n")
+
+    i = 0
+    #Skriver ut lista med bilar
+    for bil in billista:
+        print(str(i+1) + " : " + bil.fabrikat + " : " +bil.color+ ", anta i lager: " + str(bil.lager) + " st")
 
     #Avsluta programmet
     go = input("\n Vill du avsluta programmet? j/n ")
